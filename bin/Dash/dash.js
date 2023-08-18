0f3309c8a2e0a6e50ead064236fc8ca0589d1c16
KolibriKing <111354183+KolibriKing@users.noreply.github.com>
2023-08-18 10:29:32 +0000

    Pending changes exported from your codespace

---
+++ bin/Dash/dash.js
@@ -0,0 +1,36 @@
+// /bin/Dash/dash.js
+
+const dashList = ["term.print", "term.println", "term.printlnc"]
+
+function dash$(fullCommand) {
+    // Get short command
+    var shortCommand = '';
+    for (char of fullCommand) {
+        if (!(char === ' ')) {
+            shortCommand = shortCommand + char;
+        } else {
+            break;
+        }
+    }
+    if (dashList.includes(shortCommand)) {
+        switch (shortCommand) {
+            case 'term.print':
+                var output = eval(fullCommand.slice(11));
+                $('div.global.screen.terminal.text').append(`<span class="global screen terminal text">${output}</span>`);
+                break;
+            case 'term.println':
+                var output = fullCommand.slice(13);
+                $('div.global.screen.terminal.text').append(`<p class="global screen terminal text">${output}</p>`);
+                break;
+            case 'term.printlnc':
+                var color = fullCommand.slice(14, 22);
+                var output = eval(fullCommand.slice(22));
+                $('div.global.screen.terminal.text').append(`<p class="global screen terminal textc" style="color:${color};">${output}</p>`);
+                break;
+            default:
+                break;
+        }
+    } else {
+        console.error(`[Dash][ReportError] Command '${fullCommand}' wasn't executed: command '${shortCommand}' not found`);
+    }
+}
---
+++ sys/boot/bootLoad.js
@@ -0,0 +1,44 @@
+// /sys/boot/bootLoad.js
+
+// Detect boot type
+var BootType = 'n';
+//This used to print time after message in console
+let Time = new Date();
+
+if ('DeepOS.BootType' in localStorage) {
+    BootType = localStorage.getItem('DeepOS.BootType');
+    console.info(`[DeepOS][bootDetect.js][Info] BootType detected, sending ...[${Time.getTime()}]`);
+} else {
+    console.info(`[DeepOS][bootDetect.js][Info] BootType not detected, setting up BootType [${Time.getTime()}]`);
+    localStorage.setItem('DeepOS.BootType', 'terminal');
+    BootType = 'terminal';
+}
+// The main boot process
+$(document).ready(function() {
+    // Check boot type
+    if (BootType = 'terminal') {
+        // Boot into terminal session
+        // Create terminal elements
+        $('div.global.screen').append('<div class="global screen terminal container"></div>');
+        $('div.global.screen.terminal.container').append('<div class="global screen terminal text"></div>');
+        $('div.global.screen.terminal.container').append('<input class="div global screen terminal">');
+        // Focus on input field
+        $('input.global.screen.terminal').focus();
+        // Detect when keys pressed
+        $('input.global.screen.terminal').keyup(function (e){
+            $(this).attr('size', $(this).val().length);
+            // Detect if enter pressed
+            if(e.keyCode == 13){
+                // Execute command from input
+                dash$(document.querySelector('input.global.screen.terminal').value)
+            }
+          });
+          // Detect when mouse pressed
+          $(document).click(function(e) {
+            if (e.button == 0) {
+                // Focus on input to prevent bug
+                $('input.global.screen.terminal').focus();
+            }
+        });
+    }
+});
