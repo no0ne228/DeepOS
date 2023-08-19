cbc9970de8c7cfaec780816421338619664f88d3
KolibriKing <111354183+KolibriKing@users.noreply.github.com>
2023-08-19 12:41:04 +0300

    Update bootLoad.js

--- sys/boot/bootLoad.js
+++ sys/boot/bootLoad.js
@@ -17,30 +17,6 @@ if ('DeepOS.BootType' in localStorage) {
 $(document).ready(function() {
     // Check boot type
     if (BootType = 'terminal') {
-        // Boot into terminal session
-        // Create terminal elements
-        $('div.global.screen').append('<div class="global screen terminal container"></div>');
-        $('div.global.screen.terminal.container').append('<div class="global screen terminal text"></div>');
-        $('div.global.screen.terminal.container').append('<input class="div global screen terminal">');
-        // Focus on input field
-        $('input.global.screen.terminal').focus();
-        // Detect when keys pressed
-        $('input.global.screen.terminal').keyup(function (e){
-            $(this).attr('size', $(this).val().length);
-            // Detect if enter pressed
-            if(e.keyCode == 13){
-                // Execute command from input
-                dash$(document.querySelector('input.global.screen.terminal').value);
-                $('input.global.screen.terminal').val('');
-            }
-         });
-        // Detect when mouse pressed
-        $(document).click(function(e) {
-            if (e.button == 0) {
-                // Focus on input to prevent bug
-                $('input.global.screen.terminal').focus();
-            }
-        });
-        $('body').append('<script src="sys/boot/startup.js"></script>')
+        $('body').append('<script src="bin/term/run.js"></script>');
     }
 });
