ace9c79649cbce234f8961f3cc0c3c585907419d
KolibriKing <111354183+KolibriKing@users.noreply.github.com>
2023-10-17 10:00:57 +0300

    Update Shell.js

--- sys/lib/Shell.js
+++ sys/lib/Shell.js
@@ -2,12 +2,14 @@ class Shell {
   constructor(shellName) {
     importScript('sysvar');
     if (shellName == 'default') {
+      this.name = sysvar.defaultShell;
       importScript('/sys/shell/' + sysvar.defaultShell + '.js');
     } else {
+      this.name = shellName;
       importScript('/sys/shell/' + shellName + '.js');
     }
   }
   exec(cmd) {
-    eval(sysvar.defaultShell + '$(' + cmd + ')');// Shell main/global functions end with dollar sign $
+    eval(this.name + '$(' + cmd + ')');// Shell main/global functions end with dollar sign $
   }
 }
