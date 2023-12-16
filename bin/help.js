b2df0e77be93baf8b504f3af44add53c44e08395
KolibriKing <maksimevstrstiev12@outlook.com>
2023-12-16 15:10:56 +0000

    Modify help command

--- bin/help.js
+++ bin/help.js
@@ -1,23 +1,10 @@
 import { std } from '/sys/lib/std.js';
-import { fs } from '/sys/lib/fs.js';
 
 window.Dash$_help = function(data) {
-  if (typeof data.argv[0] == 'undefined') {
-    std.nl();
-    std.out('help [command]');
-    std.nl();
-    std.out('printf [...]');
-  } else {
-    console.log(`Reading /usr/msg/Dash/help/${data.argv[0]}.msg`);
-    fs.readFile(`/usr/msg/Dash/help/${data.argv[0]}.msg`, function(msg) {
-      let pmsg = JSON.parse(msg);
-      for (let string of pmsg) {
-        if (string == '/nl') {
-          std.nl();
-        } else {
-          std.out(string);
-        }
-      }
-    });
-  }
+  std.nl();
+  std.out('help [...]');
+  std.nl();
+  std.out('printf [-n]');
+  std.nl();
+  std.out('clear [...]');
 }
