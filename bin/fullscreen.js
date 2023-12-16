4a0688724c0745c6a7710130851c82d6318a609f
KolibriKing <maksimevstrstiev12@outlook.com>
2023-12-16 17:12:12 +0000

    Added fullscreen command

---
+++ bin/fullscreen.js
@@ -0,0 +1,8 @@
+window.Dash$_fullscreen = function(args) {
+  console.log('debug: entering fullscreen');
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+}
