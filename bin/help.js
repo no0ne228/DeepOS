e94ff55b96bee1d723fc5aae95b0bf8256adda20
KolibriKing <maksimevstrstiev12@outlook.com>
2023-12-28 12:15:49 +0000

    Edited help command

--- bin/help.js
+++ bin/help.js
@@ -3,12 +3,18 @@
  */
 
 import { std } from '/sys/lib/std.js';
+import { fs } from '/sys/lib/fs.js';
 
-window.Dash$_help = function(data) {
+window.Dash$_help = function(args) {
+  // Print avaliable commands
   std.nl();
   std.out('help [...]');
   std.nl();
   std.out('printf [-n]');
   std.nl();
-  std.out('clear [...]');
+  std.out('clear []');
+  std.nl();
+  std.out('shutdown [-f]');
+  std.nl();
+  std.out('pkg [... ...]');
 }
