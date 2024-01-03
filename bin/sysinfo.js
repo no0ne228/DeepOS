5ace5e1651d7c3367c706bae9509ea40da574e31
KolibriKing <111354183+KolibriKing@users.noreply.github.com>
2024-01-03 18:49:29 +0000

    Add ysinfo command

---
+++ bin/sysinfo.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2023 KolibriKing
+ */
+
+import { std } from '/sys/lib/std.js';
+import { fs } from '/sys/lib/fs.js';
+
+window.Dash$_sysinfo = function(args) {
+  // Arguments shorthand
+  const argv = args.argv;
+  
+  switch (argv[0]) {
+    case '-a':
+      // Print user agent
+      std.out(window.navigator.userAgent);
+      break;
+    default:
+      fs.readFile('/sys/data/ver.txt', function(version) {
+        std.out(`DeepOS ${version} ${window.navigator.userAgent}`);
+      });
+  }
+}
