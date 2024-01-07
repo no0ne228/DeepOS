432792a4a878f7973bd9a212f7c11469837429b0
KolibriKing <maksimevstrstiev12@outlook.com>
2024-01-07 10:43:00 +0000

    Improve mkdir command

--- bin/mkdir.js
+++ bin/mkdir.js
@@ -10,5 +10,23 @@ window.Dash$_mkdir = function(args) {
   const argv = args.argv;
   vfs$mkdir(argv[0], argv[1], function(status) {
     console.log('debug: mkdir status code is ' + status);
+    switch (status) {
+      case 1:
+        std.out('mkdir: ' + argv[0] + ': directory not found');
+        break;
+      case 2:
+        std.out('mkdir: ' + argv[0] + ': not a directory');
+        break;
+      case 4:
+        std.out('mkdir: directory name cannot contain slashes \'/\'');
+        break;
+      case 5:
+        std.out('mkdir: ' + argv[0] + ': directory already exists');
+        break;
+      default:
+        std.out('mkdir: unknown code: ' + status);
+        break;
+    }
+    std.nl();
   });
 }
