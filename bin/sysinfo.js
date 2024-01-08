38323e2cbd50f2fe16d0c70a1753537d240f9c15
KolibriKing <111354183+KolibriKing@users.noreply.github.com>
2024-01-08 16:07:55 +0000

    Fixed text output in sysinfo.js

--- bin/sysinfo.js
+++ bin/sysinfo.js
@@ -8,30 +8,35 @@ import { fs } from '/sys/lib/fs.js';
 window.Dash$_sysinfo = function(args) {
   // Arguments shorthand
   const argv = args.argv;
+  stdio.nl();
   switch (argv[0]) {
     case '-a':
       // Print user agent
       stdio.out(window.navigator.userAgent);
+      stdio.nl();
       break;
       case '--agent':
       // Print user agent
       stdio.out(window.navigator.userAgent);
+      stdio.nl();
       break;
     case '-v':
       fs.readFile('/sys/data/ver.txt', function(version) {
         stdio.out('DeepOS' + version);
+        stdio.nl();
       });
       break;
-      case '--version':
+    case '--version':
       fs.readFile('/sys/data/ver.txt', function(version) {
-        stdio.out('DeepOS' + version);
+        stdio.out('DeepOS ' + version);
+        stdio.nl();
       });
       break;
     default:
       fs.readFile('/sys/data/ver.txt', function(version) {
         stdio.out(`DeepOS ${version} ${window.navigator.userAgent}`);
+        stdio.nl();
       });
       break;
   }
-  stdio.nl();
 }
