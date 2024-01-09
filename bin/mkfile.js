bf5e6580746833e39ed37c41c0c5666dc3dd8916
KolibriKing <kolibriking12@gmail.com>
2024-01-09 19:36:38 +0000

    mkfile argv fix

--- bin/mkfile.js
+++ bin/mkfile.js
@@ -5,7 +5,7 @@
 //import { vfs$mkfile } from '/sys/lib/vfs.js';
 
 window.Dash$_mkfile = function(args) {
-  const argv = args.argv();
+  const argv = args.argv;
   /*
   vfs$mkfile(argv, function(status) {
     switch (status) {
