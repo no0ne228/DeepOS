86114e241ed24d45c6bf512dec5c78af045526b9
KolibriKing <maksimevstrstiev12@outlook.com>
2024-01-09 19:31:26 +0000

    Started making mkfile command

---
+++ bin/mkfile.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2024 KolibriKing
+ */
+
+//import { vfs$mkfile } from '/sys/lib/vfs.js';
+
+window.Dash$_mkfile = function(args) {
+  const argv = args.argv();
+  /*
+  vfs$mkfile(argv, function(status) {
+    switch (status) {
+      ...
+    }
+  });
+  */
+}
