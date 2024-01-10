f314890c5807964ee3f46068223705b219079ff9
KolibriKing <kolibriking12@gmail.com>
2024-01-10 17:59:59 +0000

    Finished mkfile command

--- bin/mkfile.js
+++ bin/mkfile.js
@@ -2,15 +2,32 @@
  * Copyright 2024 KolibriKing
  */
 
-//import { vfs$mkfile } from '/sys/lib/vfs.js';
+import { stdio } from '/sys/lib/stdio.js';
+import { vfs$mkfile } from '/sys/lib/vfs.js';
 
 window.Dash$_mkfile = function(args) {
   const argv = args.argv;
-  /*
-  vfs$mkfile(argv, function(status) {
+  vfs$mkfile(argv[0], argv[1], function(status) {
+    console.log('debug: mkfile status code is ' + status);
     switch (status) {
-      ...
+      case 0:
+        break;
+      case 1:
+        stdio.out('mkfile: ' + argv[0] + ': directory not found');
+        break;
+      case 2:
+        stdio.out('mkfile: ' + argv[0] + ': not a directory');
+        break;
+      case 4:
+        stdio.out('mkfile: directory name cannot contain slashes \'/\'');
+        break;
+      case 5:
+        stdio.out('mkfile: ' + argv[0] + ': directory already exists');
+        break;
+      default:
+        stdio.out('mkfile: unknown code: ' + status);
+        break;
     }
+    stdio.nl();
   });
-  */
 }
--- sys/lib/vfs.js
+++ sys/lib/vfs.js
@@ -98,8 +98,6 @@ export function vfs$list(dir, callback) {
             }
           }
         }
-        
-        console.log('debug: vfs$list: list is ' + result);
         callback(0, result);
       } else {
         callback(2, []); // Error: given path is not a directory
@@ -144,6 +142,67 @@ export function vfs$rmdir(dir, callback) {
       });
     } else {
       callback(1); // Error: item not found
+    }
+  });
+}
+/* Create a file in a virtual filesystem */
+export function vfs$mkfile(dest, dir, callback) {
+  /* note: dir is file name */
+  vfs$checkRoot(); // Check root in virtual filesystem
+  fs.readFile('/sys/cfg/vfs_prefix.txt', function(prefix) { // Read vfs prefix
+    console.log('debug: vfs_prefix is ' + prefix);
+    if (`${prefix}:${dest}` in localStorage) { // If file destination exists
+      // Destination folder data
+      let dest$ = JSON.parse(localStorage.getItem(`${prefix}:${dest}`));
+      if (dest$.type === 'dir') { // If destination is a directory
+        if (dest$.rights.includes('W')) { // If user has access to write to destination folder
+          if (!dir.includes('/')) { // If new file name not contains '/'
+            if (!(dest === '/')) {
+              if (!(`${prefix}:${dest}/${dir}` in localStorage)) { // If folder don't exist already
+                localStorage.setItem(`${prefix}:${dest}/${dir}`, JSON.stringify({
+                  "type": "file",
+                  "parent": dest,
+                  "rights": ["R", "W", "D"],
+                  "name": `${dest}/${dir}`,
+                  "sname": dir
+                })); // Create file object in localStorage
+                GLOBAL_VFS_TMPSTATUS = 0; // Status: done
+                callback(0);
+              } else {
+                GLOBAL_VFS_TMPSTATUS = 5; // Error: file already exists
+                callback(5);
+              }
+            } else {
+              if (!(`${prefix}:/${dir}` in localStorage)) {
+                localStorage.setItem(`${prefix}:/${dir}`, JSON.stringify({
+                  "type": "file",
+                  "parent": dest,
+                  "rights": ["R", "W", "D"],
+                  "name": '/' + dir,
+                  "sname": dir
+                })); // Create file object in localStorage
+                GLOBAL_VFS_TMPSTATUS = 0; // Status: done
+                callback(0);
+              } else {
+                GLOBAL_VFS_TMPSTATUS = 5; // Error: file already exists
+                callback(5);
+              }
+            }
+          } else {
+            GLOBAL_VFS_TMPSTATUS = 4; // Error: file name contains invalid characters
+            callback(4);
+          }
+        } else {
+          GLOBAL_VFS_TMPSTATUS = 3; // Error: permission denied
+          callback(3);
+        }
+      } else {
+        GLOBAL_VFS_TMPSTATUS = 2; // Error: given path is not a directory
+        callback(2);
+      }
+    } else {
+      GLOBAL_VFS_TMPSTATUS = 1; // Error: destination directory not found
+      callback(1);
     }
   });
 }
