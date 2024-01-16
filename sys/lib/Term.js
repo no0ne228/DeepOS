71258926b3d234eeb67fad3fe1fcb1f39026001b
KolibriKing <kolibriking12@gmail.com>
2024-01-16 09:34:35 +0000

    bugfix: changed sys/lib/Term.js std variables to stdio variables

--- sys/lib/Term.js
+++ sys/lib/Term.js
@@ -6,12 +6,12 @@
 
 export const Term = {
   create: function(holder) {
-    GLOBAL_STD_TERM = 'div#term';
-    GLOBAL_STD_TERM_TEXT = `${holder}_text`;
+    GLOBAL_STDIO_TERM = 'div#term';
+    GLOBAL_STDIO_TERM_TEXT = `${holder}_text`;
     console.log('Initialized terminal at ' + holder);
   },
   exists: function() {
-    if (GLOBAL_STD_TERM != '' && GLOBAL_STD_TERM_TEXT != '') {
+    if (GLOBAL_STDIO_TERM != '' && GLOBAL_STD_TERM_TEXT != '') {
       return true;
     } else {
       return false;
