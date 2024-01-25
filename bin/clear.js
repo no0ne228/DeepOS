26b035ac387878e9cf723c41c045eb83c1546b89
KolibriKing <kolibriking12@gmail.com>
2024-01-25 06:42:55 +0000

    feature: add clear --help, -h

--- bin/clear.js
+++ bin/clear.js
@@ -5,7 +5,16 @@
 import { stdio } from '/sys/lib/stdio.js';
 
 window.Dash$_clear = function(args) {
-  /*This command doesn't support arguments*/
-
-  stdio.clear();
+  const argv = args.argv;
+  if (argv[0] == '--help' || argv[0] == '-h') {
+    stdio.out('clear [args]');
+    stdio.nl();
+    stdio.nl();
+    stdio.out('Clear the terminal');
+    stdio.nl();
+    stdio.nl();
+    stdio.out('-h, --help: show this messags');
+  } else {
+    stdio.clear();
+  }
 }
