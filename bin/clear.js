931fcfce8d37a1d215fcaee26c504ac63152ea23
KolibriKing12 <maksimevstratiev12@outlook.com>
2024-02-03 09:45:02 +0200

    update: update newlines in clear command

--- bin/clear.js
+++ bin/clear.js
@@ -9,10 +9,8 @@ window.Dash$_clear = function(args) {
   if (argv[0] == '--help' || argv[0] == '-h') {
     stdio.out('clear [args]');
     stdio.nl();
-    stdio.nl();
     stdio.out('Clear the terminal');
-    stdio.nl();
-    stdio.nl();
+    stdio.nl(1);
     stdio.out('-h, --help: show this message');
   } else {
     stdio.clear();
