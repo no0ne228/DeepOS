352c449e38354b319056e8acd79c284fffed7acf
KolibriKing12 <maksimevstratiev12@outlook.com>
2024-02-03 10:00:33 +0200

    feature: add clear -s, --selector

--- bin/clear.js
+++ bin/clear.js
@@ -12,6 +12,10 @@ window.Dash$_clear = function(args) {
     stdio.out('Clear the terminal');
     stdio.nl(1);
     stdio.out('-h, --help: show this message');
+    stdio.nl();
+    stdio.out('-s, --selector <selector>: clear this instead of GLOBAL_STDIO_TERM_TEXT');
+  } else if (argv[0] === '-s' || argv[0] === '--selector'){
+    stdio.clearsel(argv[1]);  
   } else {
     stdio.clear();
   }
