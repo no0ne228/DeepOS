6015034400a20ca33f998982fd7fdfdef5c69b26
KolibriKing <kolibriking12@gmail.com>
2024-02-03 13:08:48 +0000

    update: update dc -h, --help

--- bin/clear.js
+++ bin/clear.js
@@ -15,7 +15,7 @@ window.Dash$_clear = function(args) {
     stdio.nl();
     stdio.out('-s, --selector <selector>: clear this instead of GLOBAL_STDIO_TERM_TEXT');
   } else if (argv[0] === '-s' || argv[0] === '--selector'){
-    stdio.clearsel(argv[1]);  
+    stdio.clearsel(argv[1]);
   } else {
     stdio.clear();
   }
--- bin/dc.js
+++ bin/dc.js
@@ -7,61 +7,61 @@ import { vfs$list, vfs$get } from '/sys/lib/vfs.js';
 
 window.Dash$_dc = function(args) {
   if (args.argv[0] != '-h' && args.argv[0] != '--help') {
-  if (args.argv[0] != undefined) {
-  vfs$list(args.argv[0], function(status, list) {
-    switch (status) {
-      case 0:
-        for (let dir of list) {
-          vfs$get(dir, function(item) {
-            if (item.type == 'dir') {
-              stdio.fout(item.sname, 'b');
-            } else {
-              stdio.out(item.sname);
+    if (args.argv[0] != undefined) {
+      vfs$list(args.argv[0], function(status, list) {
+        switch (status) {
+          case 0:
+            for (let dir of list) {
+              vfs$get(dir, function(item) {
+                if (item.type == 'dir') {
+                  stdio.fout(item.sname, 'b');
+                } else {
+                  stdio.out(item.sname);
+                }
+                stdio.nl();
+              });
             }
+            break;
+          case 1:
+            stdio.out('dc: ' + args.argv[0] + ': directory not fuond');
             stdio.nl();
-          });
+            break;
+          case 2:
+            stdio.out('dc: ' + args.argv[0] + ': not a directory');
+            stdio.nl();
+            break;
         }
-        break;
-      case 1:
-        stdio.out('dc: ' + args.argv[0] + ': directory not fuond');
-        stdio.nl();
-        break;
-      case 2:
-        stdio.out('dc: ' + args.argv[0] + ': not a directory');
-        stdio.nl();
-        break;
-      }
-  });
-  } else {
-   vfs$list(GLOBAL_VFS_DIR, function(status, list) {
-    switch (status) {
-      case 0:
-        for (let dir of list) {
-          vfs$get(dir, function(item) {
-            if (item.type == 'dir') {
-              stdio.fout(item.sname, 'b');
-            } else {
-              stdio.out(item.sname);
+      });
+    } else {
+      vfs$list(GLOBAL_VFS_DIR, function(status, list) {
+        switch (status) {
+          case 0:
+            for (let dir of list) {
+              vfs$get(dir, function(item) {
+                if (item.type == 'dir') {
+                  stdio.fout(item.sname, 'b');
+                } else {
+                  stdio.out(item.sname);
+                }
+                stdio.nl();
+              });
             }
+            break;
+          case 1:
+            stdio.out('dc: ' + args.argv[0] + ': directory not fuond');
             stdio.nl();
-          });
+            break;
+          case 2:
+            stdio.out('dc: ' + args.argv[0] + ': not a directory');
+            stdio.nl();
+            break;
         }
-        break;
-      case 1:
-        stdio.out('dc: ' + args.argv[0] + ': directory not fuond');
-        stdio.nl();
-        break;
-      case 2:
-        stdio.out('dc: ' + args.argv[0] + ': not a directory');
-        stdio.nl();
-        break;
-      }
-  });
-  }
+      });
+    }
   } else if (args.argv[0] == '-h' || args.argv[0] == '--help') {
     stdio.out('dc [directory]');
-    stdio.nl();
-    stdio.nl();
+    stdio.out('Show contents of a directory');
+    stdio.nl(1);
     stdio.out('--help, -h: displays this help message');
   }
 }
