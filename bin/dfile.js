659699cf3fb9a663806afe93246b8e918b22715a
KolibriKing <kolibriking12@gmail.com>
2024-02-04 13:43:50 +0000

    feature: add dfile --help, -h

--- bin/dfile.js
+++ bin/dfile.js
@@ -7,20 +7,28 @@ import { stdio } from '/sys/lib/stdio.js';
 
 window.Dash$_dfile = function(args) {
   const argv = args.argv;
-  vfs$rmfile(argv[0], function(status) {
-    switch (status) {
-      case 1:
+  if (argv[0] != '--help' && argv[0] != '-h') {
+    vfs$rmfile(argv[0], function(status) {
+      switch (status) {
+        case 1:
           stdio.out('dfile: ' + args.argv[0] + ': file not found');
           stdio.nl();
           break;
-      case 2:
-        stdio.out('dfile: ' + args.argv[0] + ': not a file');
-        stdio.nl();
-        break;
-      case 3:
-        stdio.out('dfile: ' + args.argv[0] + ': permission denied');
-        stdio.nl();
-        break;
-    }
-  });
+        case 2:
+          stdio.out('dfile: ' + args.argv[0] + ': not a file');
+          stdio.nl();
+          break;
+        case 3:
+          stdio.out('dfile: ' + args.argv[0] + ': permission denied');
+          stdio.nl();
+          break;
+      }
+    });
+  } else {
+    stdio.out('dfile [args]');
+    stdio.out('Delete a file');
+    stdio.nl(1);
+    stdio.out('--help, -h: display this help message');
+    stdio.nl();
+  }
 }
