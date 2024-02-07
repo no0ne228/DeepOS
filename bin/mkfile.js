e94b1a18ab93dbf17b850ddeacf0efa45f74f53f
KolibriKing <kolibriking12@gmail.com>
2024-02-07 06:21:10 +0000

    feature: add mkfile --help, -h

--- bin/mkfile.js
+++ bin/mkfile.js
@@ -7,27 +7,38 @@ import { vfs$mkfile } from '/sys/lib/vfs.js';
 
 window.Dash$_mkfile = function(args) {
   const argv = args.argv;
-  vfs$mkfile(argv[0], argv[1], function(status) {
-    console.log('debug: mkfile status code is ' + status);
-    switch (status) {
-      case 0:
-        break;
-      case 1:
-        stdio.out('mkfile: ' + argv[0] + ': directory not found');
-        break;
-      case 2:
-        stdio.out('mkfile: ' + argv[0] + ': not a directory');
-        break;
-      case 4:
-        stdio.out('mkfile: directory name cannot contain slashes \'/\'');
-        break;
-      case 5:
-        stdio.out('mkfile: ' + argv[0] + ': directory already exists');
-        break;
-      default:
-        stdio.out('mkfile: unknown code: ' + status);
-        break;
-    }
+  switch(argv[0]) {
+  case '--help', '-h':
+    stdio.out('mkfile [-h]');
     stdio.nl();
-  });
+    stdio.out('Create a file');
+    stdio.nl(1);
+    stdio.out('--help, -h: display this help message');
+    break;
+  default:
+    vfs$mkfile(argv[0], argv[1], function(status) {
+      console.log('debug: mkfile status code is ' + status);
+      switch (status) {
+        case 0:
+          break;
+        case 1:
+          stdio.out('mkfile: ' + argv[0] + ': directory not found');
+          break;
+        case 2:
+          stdio.out('mkfile: ' + argv[0] + ': not a directory');
+          break;
+        case 4:
+          stdio.out('mkfile: directory name cannot contain slashes \'/\'');
+          break;
+        case 5:
+          stdio.out('mkfile: ' + argv[0] + ': directory already exists');
+          break;
+        default:
+          stdio.out('mkfile: unknown code: ' + status);
+          break;
+      }
+      stdio.nl();
+    });
+    break;
+  }
 }
