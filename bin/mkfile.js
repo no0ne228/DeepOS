767d3738eb5a232a73b9d5ee8b2f8a5cdc82ed14
KolibriKing <kolibriking12@gmail.com>
2024-02-07 06:40:42 +0000

    feature: add mkfile --quiet, -q

--- bin/mkfile.js
+++ bin/mkfile.js
@@ -9,36 +9,40 @@ window.Dash$_mkfile = function(args) {
   const argv = args.argv;
   switch(argv[0]) {
   case '--help', '-h':
-    stdio.out('mkfile [-h]');
+    stdio.out('mkfile [-hq]');
     stdio.nl();
     stdio.out('Create a file');
     stdio.nl(1);
     stdio.out('--help, -h: display this help message');
+    stdio.nl();
+    stdio.out('--quiet, -q: don\'t show any warnings or errors');
+    stdio.nl();
     break;
   default:
-    vfs$mkfile(argv[0], argv[1], function(status) {
-      console.log('debug: mkfile status code is ' + status);
-      switch (status) {
-        case 0:
-          break;
-        case 1:
-          stdio.out('mkfile: ' + argv[0] + ': directory not found');
-          break;
-        case 2:
-          stdio.out('mkfile: ' + argv[0] + ': not a directory');
-          break;
-        case 4:
-          stdio.out('mkfile: directory name cannot contain slashes \'/\'');
-          break;
-        case 5:
-          stdio.out('mkfile: ' + argv[0] + ': directory already exists');
-          break;
-        default:
-          stdio.out('mkfile: unknown code: ' + status);
-          break;
-      }
-      stdio.nl();
-    });
-    break;
+    if (!argv.includes('--quiet') && !argv.includes('-q')) {
+      vfs$mkfile(argv[0], argv[1], function(status) {
+        console.log('debug: mkfile status code is ' + status);
+        switch (status) {
+          case 0:
+            break;
+          case 1:
+            stdio.out('mkfile: ' + argv[0] + ': directory not found');
+            break;
+          case 2:
+            stdio.out('mkfile: ' + argv[0] + ': not a directory');
+            break;
+          case 4:
+            stdio.out('mkfile: directory name cannot contain slashes \'/\'');
+            break;
+          case 5:
+            stdio.out('mkfile: ' + argv[0] + ': directory already exists');
+            break;
+          default:
+            stdio.out('mkfile: unknown code: ' + status);
+            break;
+        }
+        stdio.nl();
+      });
+    }
   }
 }
