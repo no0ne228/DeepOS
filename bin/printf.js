58cbade387ccb606126a79519a77689a4a3722b2
KolibriKing <kolibriking12@gmail.com>
2024-02-08 04:44:43 +0000

    feature: pkg --help, -h

--- bin/printf.js
+++ bin/printf.js
@@ -2,13 +2,27 @@
  * Copyright 2024 KolibriKing
  */
 
-import { stdio } from "/sys/lib/stdio.js"
+import { stdio } from '/sys/lib/stdio.js';
 
 window.Dash$_printf = function(data) {
-  if (data.argv[0] == '-n') {
-    stdio.nl();
-    stdio.out(data.argv[1]);
-  } else {
-    stdio.out(data.argv[0])
+  switch (args.argv[0]) {
+    case'--help', '-h':
+      stdio.out('printf [-hn]');
+      stdio.nl();
+      stdio.out('Print out text');
+      stdio.nl(1);
+      stdio.out('--help, -h: display this help message');
+      stdio.nl();
+      stdio.out('--newline, -n: output a new line after text');
+      stdio.nl();
+      break;
+    default:
+      if (data.argv.includes('-n') || data.argv.includes('--newline')) {
+        stdio.out(data.argv[1]);
+        stdio.nl();
+      } else {
+        stdio.out(data.argv[0]);
+      }
+      break;
   }
 }
