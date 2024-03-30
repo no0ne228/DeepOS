29d0d0716cab41fc7e378f27933943a7b86da25d
noOne228 <163328629+noOne228@users.noreply.github.com>
2024-03-30 12:40:19 +0000

    feature: now running Dash Terminal window on desktop loading

--- bin/desktop.js
+++ bin/desktop.js
@@ -1,12 +1,29 @@
-import { Desktop } from "/sys/lib/Desktop.js"
+import { Desktop, Window } from "/sys/lib/Desktop.js"
 
 window.Dash$_desktop = function(args) {
   GLOBAL_BOOT_STATUS = 'DEFAULT_GRAPHICAL';
   document.querySelector('div#term').remove();
-
+  document.querySelector('body').style.cursor = 'default';
+  
   var desktop = new Desktop({
-    bg_type: 'fill',
-    bg: 'gray'
+    bg: {
+      type: 'fill',
+      fill: 'lightgray'
+    },
+    topbar: {
+      fill: 'rgb(98, 98, 98)'
+    }
   });
   desktop.init();
+  var window = new Window({
+    width: '50vw',
+    height: '25vh',
+    src: '/index.html',
+    pos: {
+      top: '25vh',
+      left: '25vw'
+    },
+    title: 'Dash Terminal'
+  });
+  window.init();
 }
